2fff8dabcd122ea7a5590de6f7001758efe3327b
ljramirez <ramirezl0415@students.bowiestate.edu>
2023-09-27 13:20:09 -0400

    Labs Classwork

--- Lab 2b.py
+++ Lab 2b.py
@@ -7,4 +7,12 @@ multiply=number1*number2*number3*number4
 subtract=number4-number3-number2-number1
 division=(number4/number3)
 print(add,multiply,subtract,division)
-
+number1= 13
+number2= 12
+number3= 11
+number4= 10
+add=number1+number2+number3+number4
+multiply=number1*number2*number3*number4
+subtract=number4-number3-number2-number1
+division=(number4/number3)
+print(add,multiply,subtract,division)
