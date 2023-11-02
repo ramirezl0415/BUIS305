2037cb67d3175f7506c3b4ccec4448e02acfdc98
ljramirez <ramirezl0415@students.bowiestate.edu>
2023-11-02 16:27:13 -0400

    Problem 3

---
+++ Problem3.py
@@ -0,0 +1,8 @@
+number1= input('Enter number 1')
+number2= input('Enter number 2')
+number3= input('Enter number 3')
+number4=input('Enter number 4')
+number5=input('Enter number 5')
+sum = int(number1) + int(number2) + int(number3) + int(number4) + int(number5)
+average = sum/5
+print('Sum', sum,'Average', average)
